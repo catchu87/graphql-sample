f224ef6c610d1c6b117db96c1d2bab1741e42713
catchu87 <catchu87@gmail.com>
2019-08-15 22:11:24 +0900

    컨텐츠 데이터 분리 및 수정.

---
+++ resources/dummy.js
@@ -0,0 +1,13 @@
+//dummy data
+const dummyBooks = [
+    { title: "유아 식판식", author: "김주연" },
+    { title: "천년의 질문1", author: "조정래" },
+    { title: "천년의 질문2", author: "조정래" },
+    { title: "천년의 질문3", author: "조정래" },
+    { title: "직지1", author: "김진명" },
+    { title: "직지2", author: "김진명" },
+    { title: "빨간 머리 앤", author: "루시 모드 몽고베리" },
+    { title: "아몬드", author: "손원평" },
+];
+
+export default dummyBooks;
